9d84a6185ee5703ec1eeef567ebe30db11babb6d
christiankleber <kc7829@mci4me.at>
2024-04-17 11:12:10 +0200

    test

--- my_functions.py
+++ my_functions.py
@@ -8,7 +8,7 @@ def estimate_max_hr(age_years : int , sex : str) -> int:
     max_hr_bpm = 226 - 1.0 *  age_years
   else:
     # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
-    max_hr_bpm  = input("Enter maximum heart rante:")
+    max_hr_bpm  = input("Enter maximum heart rate:")
   return int(max_hr_bpm)
 
 def build_person(first_name, last_name, sex, age) -> dict:
